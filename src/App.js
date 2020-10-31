397307aa25507c1da50a91f89aae12dd9627897a
abernier <antoine.bernier@gmail.com>
2020-10-31 08:04:26 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -4,39 +4,57 @@ import './App.css';
 
 import json from './contacts.json';
 
-function App() {
-  const contacts = json.slice(0,5)
+class App extends React.Component {
+  state = {
+    contacts: json.slice(0, 5)
+  }
+
+  addHandler = () => {
+    const randContact = json[Math.floor(Math.random()*json.length)];
+
+    this.setState({
+      contacts: [...this.state.contacts, randContact]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
   
-  return (
-    <div className="App">
-      <h1>IronContacts</h1>
+        <p>
+            <button onClick={e => this.addHandler()}>Add new contact</button>
+          </p>
+  
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((contact, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <img src={contact.pictureUrl} style={{width: '100px'}} />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+  
+        
+      </div>
+    );
+  }
 
-      <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map((contact, index) => {
-            return (
-              <tr key={index}>
-                <td>
-                  <img src={contact.pictureUrl} style={{width: '100px'}} />
-                </td>
-                <td>{contact.name}</td>
-                <td>{contact.popularity}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-
-      
-    </div>
-  );
+  
 }
 
 export default App;
