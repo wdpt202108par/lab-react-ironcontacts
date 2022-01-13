074c782fd2089c9d07f0830e50d0eb4dd4491c9f
Cindy Confiant <cindy.confiant@gmail.com>
2022-01-13 21:45:13 +0100

    iterations 1 and 2 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import contacts from "./contacts.json";
+
+const five= [...contacts.slice(0, 5)];
+
+class Random extends React.Component {
+  state = {
+    contacts: five,
+  }
+
+  addNewRandom = (event) => {
+    const random = contacts[Math.floor(Math.random() * contacts.length)];
+    console.log('random: ', random)
+    this.setState ({
+      contacts : [...this.state.contacts, {...random}]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1>IronContacts</h1>
+      <button onClick={this.addNewRandom}>Add Random Contact</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map(contact => {
+              return (
+                <tr key={contact.id}>
+                  <td><img src={contact.pictureUrl} alt="contactpic"></img></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity.toFixed(2)}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Random />
+    </>
+  )
 }
 
 export default App;
