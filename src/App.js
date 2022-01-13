5455ab8e2dc7c23bc877bd044c871372e56457e9
Lea Beaulieu <beaulieu.lea@free.fr>
2022-01-13 21:36:00 +0100

    did iteration 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import contacts from "./contacts.json";
+
+const numbers = [1, 2, 3, 4, 5];
+ 
+// array of list item HTML elements that needs to be displayed
+export const listItems = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>, <li>5</li>];
+
+//export const listItems = numbers.map((el, i) => <li key={i}>{el}</li>); // [<li>1</li>, ....]
+
+// recupérer les 5 premiers contacts
+
+const actors = contacts.splice(0, 5)
+
+class Actor extends Component{
+  state = {
+    actors: actors
+  }
+
+  render() {
+    return (
+      <>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {
+            this.state.actors.map(el => { return (
+            
+              <tr>
+                <td>{el.name}</td>
+                <td>{el.popularity}</td>
+              </tr> )
+            })
+          }
+        </table>
+      </>
+          )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Actor />
+
       </header>
     </div>
   );
