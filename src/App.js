fbf23258727019bed6f2042d8a186a18b833e49d
Yooyoo56 <tak.yoojeong@gmail.com>
2022-01-15 11:35:38 +0100

    iteration 2 in process

--- src/App.js
+++ src/App.js
@@ -1,59 +1,69 @@
-
-import { Component } from 'react';
-import './App.css';
+import logo from "./logo.svg";
+import { Component } from "react";
+import "./App.css";
 import contacts from "./contacts.json";
 
- 
-// array of list item HTML elements that needs to be displayed
-export const listItems = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>, <li>5</li>];
-
-//export const listItems = numbers.map((el, i) => <li key={i}>{el}</li>); // [<li>1</li>, ....]
-
 // recupérer les 5 premiers contacts
+const actors = contacts.splice(0, 5);
 
-const actors = contacts.splice(0, 5)
+//change the App function to the class component
+class App extends Component {
+	state = {
+		actors: actors,
+		contacts: contacts,
+		contactsEnd: contacts.slice(5, contacts.length), // [ {}, {}, {}, {}, {} ]
+	};
 
-class Actor extends Component{
-  state = {
-    actors: actors
-  }
-
-  render() {
-    return (
-      <>
-        <table>
-          <tr>
-            <th>picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-          {
-            this.state.actors.map(el => { return (
-            
-              <tr>
-                <td><img src={el.pictureUrl} alt="actor" /></td>
-                <td>{el.name}</td>
-                <td>{el.popularity}</td>
-              </tr> )
-            })
-          }
-        </table>
-      </>
-          )
-  }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-         <h1>Iron Contacts</h1>
-        <Actor />
-
-      </header>
-    </div>
-  );
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>Iron Contact</h1>
+					<button
+						onClick={() => {
+							console.log("coucou");
+							// 1. tirer au sort un acteur de la liste json
+							const random = Math.floor(
+								Math.random() * this.state.contacts.length
+							);
+							console.log(random);
+							// 2. faire une copie de this.state.actor
+							const contactsCopy = [...this.state.contacts];
+							console.log("copy:", contactsCopy);
+							// 3. sur cette copie on va .push 1.
+							let contactsEnd = Math.floor(Math.random()[1]);
+							contactsCopy.push(contactsEnd);
+							//	console.log("push:", );
+						}}
+					>
+						Add Random Contact
+					</button>
+					<table>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Popularity</th>
+							</tr>
+						</thead>
+						<tbody>
+							{/* [ <tr key="">, <tr key="">, <tr> ] */}
+							{this.state.actors.map((el) => {
+								return (
+									<tr key={el.id}>
+										<td>
+											<img src={el.pictureUrl} alt="actor" />
+										</td>
+										<td>{el.name}</td>
+										<td>{el.popularity}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
